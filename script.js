3c14ae4612d1538efad7490782fb69bd67babeb7
Gustavo Correia <gucorreia2901@gmail.com>
2022-09-07 15:23:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() =>{
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = mario.offsetTop;
+    
+    
+    if (pipePosition < 200 && pipePosition > 120 &&  marioPosition > 291) {
+      
+      pipe.style.animation = 'none';
+      pipe.style.left = `${pipePosition}px`
+      mario.style.animation = 'none';
+      mario.style.top = `${marioPosition}px`
+
+      mario.src = './game/game-over.png'
+      mario.style.width = '50px'
+      mario.style.marginleft = '50px'
+      
+      clearInterval(loop);
+    }  
+
+   
+    
+
+}, 10)
+
+document.addEventListener('keydown', jump);
